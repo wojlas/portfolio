37767b751c84dfa15b9bc7d8116b514bd0a8643c
wojtek <wojlas93@gmail.com>
2024-04-15 22:29:11 +0200

    breadcrumbs concept

--- frontend/portfolio/src/app/shared/portfolio-page/portfolio-page.component.ts
+++ frontend/portfolio/src/app/shared/portfolio-page/portfolio-page.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SidebarComponent } from "../../views/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { BreadcrumbsComponent } from "../../ui/breadcrumbs/breadcrumbs.component";
 
 @Component({
     selector: 'app-portfolio-page',
@@ -8,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './portfolio-page.component.html',
     styleUrl: './portfolio-page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [SidebarComponent, RouterOutlet]
+    imports: [SidebarComponent, RouterOutlet, BreadcrumbsComponent]
 })
 export class PortfolioPageComponent {
 
