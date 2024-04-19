3ea19c6a71fdf757650ce7c2f9b7139a280b4eef
wojtek <wojlas93@gmail.com>
2024-04-19 23:58:42 +0200

    #16 filters connected in portfolio view

--- frontend/portfolio/src/app/submodules/projects/projects.component.ts
+++ frontend/portfolio/src/app/submodules/projects/projects.component.ts
@@ -16,7 +16,28 @@ export class ProjectsComponent {
   public projects = signal<ISimpleProject[]>([]);
 
   public constructor(private readonly _activatedRoute: ActivatedRoute) {
-    this.projects.set(this._activatedRoute.snapshot.data['data'][2])
+    this._activatedRoute.queryParams.subscribe(params => {
+      
+      if (!Object.keys(params).length) {
+        this.setProjectsList(this._activatedRoute.snapshot.data['data'][2]);
+      } else {
+        this.filterProjectsList(params['type'], +params['id']); 
+      }
+    });
   }
 
+  private setProjectsList(projects: ISimpleProject[]): void {
+    this.projects.set(projects)
+  }
+
+  private filterProjectsList(type: 'languages' | 'frameworks', id: number): void {
+    const base: ISimpleProject[] = this._activatedRoute.snapshot.data['data'][2];
+    this.setProjectsList(base.filter(project => {
+      if (type === 'languages') {
+        return project.language.some(lang => lang.id === id);
+      } else {
+        return project.framework.some(framework => framework.id === id);
+      }
+    }));
+  }
 }
