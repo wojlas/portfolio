b4584ca2d8d068ee8501b4e5d878d0448a129511
wojtek <wojlas93@gmail.com>
2024-08-06 20:58:44 +0200

    projects descrpitions, deploy 0.0.12 version

--- frontend/portfolio/src/app/core/constants/PROJECTS.ts
+++ frontend/portfolio/src/app/core/constants/PROJECTS.ts
@@ -115,64 +115,80 @@ export const PROJECTS: Record<Projects, IProject> = {
     name: 'TODO App',
     languages: [LANGUAGES.JAVA_SCRIPT, LANGUAGES.HTML, LANGUAGES.CSS],
     frameworks: [FRAMEWORKS.REACT],
-    releaseDate: '2022-01-10',
-    repositoryLink: '',
-    descriptionPl: 'TODO App',
-    descriptionEn: 'TODO App',
+    releaseDate: '2022-01-19',
+    repositoryLink: 'https://github.com/wojlas/TODO-React',
+    descriptionPl: `Lista rzeczy do zrobienia. Moja pierwsza 'większa' aplikacja napisana w React, bez użycia managera stanu, tylko podstawowe propsy.
+     Aplikacja umożliwia ustawienie listy rzeczy do zrobienia o określonych porach i z określonym priorytetem, a także możliwość odkładania zadania o 15 minut lub godzinę.`,
+    descriptionEn: `A to-do list. My first 'bigger' application written in React, without using a state manager, just basic props.
+     The application allows you to set a to-do list with specific times and priorities, and also provides the option to postpone tasks by 15 minutes or an hour.`,
   },
   [Projects.WEATHER_APP]: {
     id: 7,
     name: 'Weather App',
     languages: [LANGUAGES.HTML, LANGUAGES.CSS, LANGUAGES.JAVA_SCRIPT],
-    releaseDate: '2021-12-15',
-    repositoryLink: '',
-    descriptionPl: 'Forecast',
-    descriptionEn: 'Forecast',
+    releaseDate: '2021-11-28',
+    repositoryLink: 'https://github.com/wojlas/weatherApp',
+    descriptionPl: `Aplikacja wyświetlająca prognozę pogody na następne 5 dni. Prognoza pochodzi z zewnętrznego api, frontend został napisany w czystym JavaScript.`,
+    descriptionEn: 'An application displaying the weather forecast for the next 5 days. The forecast comes from an external API, and the frontend is written in plain JavaScript.',
   },
   [Projects.LOTTO]: {
     id: 8,
     name: 'Lotto Game',
     languages: [LANGUAGES.PYTHON],
-    releaseDate: '',
-    repositoryLink: '',
-    descriptionPl: 'Game',
-    descriptionEn: 'Game',
+    releaseDate: '2021-04-23',
+    repositoryLink: 'https://github.com/wojlas/LOTTO-game',
+    descriptionPl: `Symulacja gry Lotto napisana w Pythonie. Element większej serii mini-gierek do odpalenia w terminalu.
+     Należy podać 6 liczb od 1 do 49. Komputer losuje swoje 6 liczb, po czym informuje nas ile z nich trafiliśmy.`,
+    descriptionEn: `A Lotto game simulation written in Python. It is part of a larger series of mini-games to be run in the terminal.
+     You need to enter 6 numbers from 1 to 49. The computer then draws its 6 numbers and informs you how many you guessed correctly.`,
   },
   [Projects.GUESS_THE_NUMBER]: {
     id: 9,
     name: 'Guess the number',
     languages: [LANGUAGES.PYTHON],
-    releaseDate: '',
-    repositoryLink: '',
-    descriptionPl: 'Game',
-    descriptionEn: 'Game',
+    releaseDate: '2021-04-23',
+    repositoryLink: 'https://github.com/wojlas/Guess-the-number',
+    descriptionPl: `Prosta gra w zgadywanki napisana w Pythonie, element większej serii mini-gierek do odpalenia w terminalu.
+    Komputer losuje liczbę od 1 do 100, naszym zadaniem jest zgadnąć tę liczbę przy jak najmniejszej liczbie prób.
+     Gra informuje nas, czy podana liczba jest mniejsza, czy wyższa niż ta wylosowana.`,
+    descriptionEn: `A simple guessing game written in Python, part of a larger series of mini-games to be run in the terminal.
+     The computer draws a number from 1 to 100, and your task is to guess the number with as few attempts as possible.
+     The game informs you whether the guessed number is lower or higher than the drawn number.`,
   },
   [Projects.GUESS_THE_NUMBER_FLASK]: {
     id: 10,
     name: 'Guess the number in Flask',
     languages: [LANGUAGES.PYTHON],
     frameworks: [FRAMEWORKS.FLASK],
-    releaseDate: '',
-    repositoryLink: '',
-    descriptionPl: 'Game',
-    descriptionEn: 'Game',
+    releaseDate: '2021-04-23',
+    repositoryLink: 'https://github.com/wojlas/Guess_the_number_in_Flask',
+    descriptionPl: `Wersja gry Guess the number wraz z interfejsem graficznym napisanym we frameworku Flask. Element większej serii mini-gierek do odpalenia w terminalu.
+    Komputer losuje liczbę od 1 do 100, naszym zadaniem jest zgadnąć tę liczbę przy jak najmniejszej liczbie prób.
+     Gra informuje nas, czy podana liczba jest mniejsza, czy wyższa niż ta wylosowana.`,
+    descriptionEn: `A version of the Guess the Number game with a graphical interface written in the Flask framework. It is part of a larger series of mini-games to be run in the terminal.
+     The computer draws a number from 1 to 100, and your task is to guess the number with as few attempts as possible.
+      The game informs you whether the guessed number is lower or higher than the drawn number.`,
   },
   [Projects.ANOTHER_GUESS_THE_NUMBER]: {
     id: 11,
     name: 'Reverted guess the number',
     languages: [LANGUAGES.PYTHON],
-    releaseDate: '',
-    repositoryLink: '',
-    descriptionPl: 'Game',
-    descriptionEn: 'Game',
+    releaseDate: '2021-04-23',
+    repositoryLink: 'https://github.com/wojlas/Another-Guess-the-number',
+    descriptionPl: `Prosta gra w zgadywanki, element większej serii mini-gierek do odpalenia w terminalu.
+    Tym razem to gracz wybiera liczbę z przedziału 1-1000 a komputer ma 10 prób aby zgadnąć jaka to liczba. Jeżeli mu się uda - wygrywa, jeżeli nie - wygrywa gracz.`,
+    descriptionEn: `A simple guessing game, part of a larger series of mini-games to be run in the terminal.
+     This time, the player chooses a number from the range 1-1000, and the computer has 10 attempts to guess the number. If the computer succeeds, it wins; if not, the player wins.`,
   },
   [Projects.DICE]: {
     id: 12,
     name: 'Dice roll',
     languages: [LANGUAGES.PYTHON],
-    releaseDate: '',
-    repositoryLink: '',
-    descriptionPl: 'Game',
-    descriptionEn: 'Game',
+    releaseDate: '2021-04-23',
+    repositoryLink: 'https://github.com/wojlas/DICE',
+    descriptionPl: `Gra symulująca rzut kością do gry. Element większej serii mini-gierek do odpalenia w terminalu.
+    Gracz wybiera liczbę rzutów, rodzaj kości i dodatkowy modyfikator, po czym gra zwraca wynik rzutu.`,
+    descriptionEn: `A game simulating a dice roll. Part of a larger series of mini-games to be run in the terminal.
+     The player chooses the number of rolls, the type of dice, and an additional modifier, and the game returns the result of the roll.`,
   }
 };
